404e664ca9513e27d58c839052e616cfa06d2823
VLZelinsky911 <zelinskyvlad991@gmail.com>
2025-04-05 01:01:52 +0300

    Added disign for sign-up

--- frontend/app/(site)/page.tsx
+++ frontend/app/(site)/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h2 className="text-4xl tex-">Hello</h2>
+    <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <h1 className="text-4xl text-blue-500">Home</h1>
     </div>
   );
 }
