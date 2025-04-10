a3ed87754beba593c7153c4ab6f9a647f119731b
VLZelinsky911 <zelinskyvlad991@gmail.com>
2025-04-10 14:20:24 +0300

    Fix

--- strapi/config/plugins.ts
+++ strapi/config/plugins.ts
@@ -1,4 +1,14 @@
-export default () => ({
-
+export default ({ env }) => ({
+  'users-permissions': {
+    config: {
+      providers: {
+        google: {
+          clientId: env('GOOGLE_CLIENT_ID'),
+          clientSecret: env('GOOGLE_CLIENT_SECRET'),
+          redirectUri: 'http://localhost:1337/api/connect/google/callback',
+        },
+      },
+    },
+  },
 });
 
