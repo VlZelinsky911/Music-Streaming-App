e8abadc6b032286e1be420745c211afb9d399660
VLZelinsky911 <zelinskyvlad991@gmail.com>
2025-04-23 23:45:15 +0300

    Fix

--- components/header/SearchBar/SearchBar.tsx
+++ components/header/SearchBar/SearchBar.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { FaTrash } from "react-icons/fa";
 import { HiOutlineSearch } from "react-icons/hi";
+import { HiOutlineInbox } from "react-icons/hi";
 
 export default function SearchBar() {
   const [query, setQuery] = useState("");
@@ -9,13 +10,15 @@ export default function SearchBar() {
     <div className="bg-black w-full flex items-center justify-center">
       <div
         className={`
-          flex items-center px-2 py-2 rounded-full
+          flex items-center px-4 py-2 rounded-full
           bg-neutral-800 transition-all duration-200
           hover:bg-neutral-700 focus-within:ring-1 focus-within:ring-white
-           w-80
+          w-80
         `}
       >
+
         <HiOutlineSearch className="mr-3 text-gray-400 w-5 h-5" />
+
         <input
           type="text"
           placeholder="What do you want to play?"
@@ -23,10 +26,15 @@ export default function SearchBar() {
           onChange={(e) => setQuery(e.target.value)}
           className="flex-1 bg-transparent text-sm text-white placeholder-gray-400 outline-none"
         />
-        {query && (
-          <button onClick={() => setQuery("")} className="ml-3">
-            <FaTrash className="text-gray-400 hover:text-white text-base" />
+
+        <div className="h-5 w-px bg-gray-600 mx-2" />
+
+        {query ? (
+          <button onClick={() => setQuery("")} className="text-gray-400 hover:text-white">
+            <FaTrash className="text-base" />
           </button>
+        ) : (
+          <HiOutlineInbox className="text-gray-400 w-5 h-5" />
         )}
       </div>
     </div>
