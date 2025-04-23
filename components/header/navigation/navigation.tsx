5c4202e6bc259ecd8fda38cb1b2498ede852722a
VLZelinsky911 <zelinskyvlad991@gmail.com>
2025-04-23 23:32:02 +0300

    Added new header

---
+++ components/header/navigation/navigation.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import React from "react";
+import { MdHomeFilled } from "react-icons/md";
+import SearchBar from "../SearchBar/SearchBar";
+
+export default function Navigation() {
+  return (
+		<div className="flex items-center gap-2">
+      <Link
+        href="/"
+        className="p-2 rounded-full bg-neutral-800 hover:bg-neutral-700 scale-105 shadow-lg transition duration-200 ease-in-out"
+      >
+        <MdHomeFilled className="text-white text-xl" />
+      </Link>
+
+      <SearchBar />
+		</div>
+  );
+}
