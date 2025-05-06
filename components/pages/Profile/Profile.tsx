96fc80217ff3a12c8b25b4aef1f65229c59d040a
VLZelinsky911 <zelinskyvlad991@gmail.com>
2025-05-06 14:08:22 +0300

    Added mini menu

--- components/pages/Profile/Profile.tsx
+++ components/pages/Profile/Profile.tsx
@@ -1,6 +1,11 @@
-import React from "react";
+"use client";
+
+import Link from "next/link";
+import React, { useState } from "react";
 
 export default function ProfilePage() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className="min-h-screen bg-black text-white flex">
       <div className="w-1/3 bg-neutral-900 p-6 space-y-6">
@@ -41,9 +46,58 @@ export default function ProfilePage() {
         </div>
 
         <div className="p-8">
-          <button className="text-white text-3xl hover:text-gray-400 transition-colors duration-300">
+          <button
+            onClick={() => setMenuOpen(!menuOpen)}
+            className=" text-white text-3xl hover:text-gray-400 active:scale-90 transition-transform duration-100 cursor-pointer"
+          >
             ⋯
           </button>
+
+          {menuOpen && (
+            <div className="absolute mt-2 py-2 px-1 w-48 bg-[#282828] text-[#DFDFDF] rounded-lg shadow-lg z-50">
+              <Link
+                href="/profile"
+                className="flex items-center px-4 py-2 hover:bg-[#3a3a3a] hover:underline transition duration-150 rounded-md"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-4 w-4 mr-2"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5M18.5 2.5l3 3L14 13l-4 1 1-4 8.5-8.5z"
+                  />
+                </svg>
+                Edit Profile
+              </Link>
+
+              <Link
+                href="/settings"
+                className="flex items-center px-4 py-2 hover:bg-[#3a3a3a] hover:underline transition duration-150 rounded-md"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-4 w-4 mr-2"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M8 16h8M8 12h8m-6 8h6a2 2 0 002-2V7a2 2 0 00-2-2H9l-2 2H5a2 2 0 00-2 2v10a2 2 0 002 2h2z"
+                  />
+                </svg>
+                Profile URL
+              </Link>
+            </div>
+          )}
         </div>
 
         <footer className="flex-grow bg-neutral-950 text-gray-400 text-sm p-6">
