38e75b4ce337d336abc3c039697cbe68b0f3b140
VLZelinsky911 <zelinskyvlad991@gmail.com>
2025-05-15 13:25:42 +0300

    Added history page

---
+++ app/(site)/(pages)/profile/history/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const metadata = {
+  title: "Wavely - Listening history",
+  description: "Wavely is a most popular music streaming service in the world",
+  icons: {
+    icon: "/wavely_logo_title_green.png",
+  },
+};
+
+export default function HistoryPage() {
+	return (
+		<div>HistoryPage</div>
+	)
+}
